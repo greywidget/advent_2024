7fa2ccdc86b4359d0bd9687f9104981c2411d23a
Craig Richards <craig@greywidget.co.uk>
2024-12-06 14:47:16 +0000

    Refactored Day 5 Puzzle 01

--- 05/puzzle05.py
+++ 05/puzzle05.py
@@ -24,41 +24,41 @@ def _import_data():
 puzzle_data = _import_data()
 
 
-class Puzzle:
-    def __init__(self, puzzle_data):
-        self.rules = self._get_rules(puzzle_data[0])
-        self._updates = puzzle_data[1]
+def next_rule(rule_data):
+    for item in rule_data:
+        first, second = item.split("|")
+        yield int(first), int(second)
 
-    def _get_rules(self, rule_data):
-        return [tuple(map(int, item.split("|"))) for item in rule_data]
 
-    def _get_middle_page(self, update):
-        pos = len(update) // 2
-        for index, item in enumerate(update.keys()):
-            if index == pos:
-                return item
+def next_update(update_data):
+    for item in update_data:
+        update = {int(value): index for index, value in enumerate(item.split(","))}
+        yield update
 
-    def next_update(self):
-        for item in self._updates:
-            update = {int(value): index for index, value in enumerate(item.split(","))}
-            yield update
 
-    def apply_rules(self, update):
-        for rule in self.rules:
-            if all(item in update.keys() for item in rule):
-                first, second = rule
-                if update[first] > update[second]:
-                    return False
-        return True
+def middle_page(update):
+    pos = len(update) // 2
+    for index, item in enumerate(update.keys()):
+        if index == pos:
+            return item
+
+
+def correctly_ordered(update, rule_data):
+    for rule in next_rule(rule_data):
+        if all(item in update.keys() for item in rule):
+            first, second = rule
+            if update[first] > update[second]:
+                return False
+    return True
 
 
 def solve(puzzle=1, puzzle_data=puzzle_data):
+    rule_data, update_data = puzzle_data
     if puzzle == 1:
-        puzzle = Puzzle(puzzle_data)
         middle_page_sum = 0
-        for update in puzzle.next_update():
-            if puzzle.apply_rules(update):
-                middle_page_sum += puzzle._get_middle_page(update)
+        for update in next_update(update_data):
+            if correctly_ordered(update, rule_data):
+                middle_page_sum += middle_page(update)
 
     return middle_page_sum
 
